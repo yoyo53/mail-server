eba3ade900014c1486349991d020200726f6673e
Yohan Villiers <94387950+yoyo53@users.noreply.github.com>
2025-04-21 19:12:08 +0200

    Fix forward-mail.sh

--- postfix/forward-mail.sh
+++ postfix/forward-mail.sh
@@ -3,4 +3,4 @@
 PID=$(pidof supervisord)
 export $(grep -v '^#' /proc/$PID/environ  | xargs -0)
 
-cat | curl -X POST "http://localhost:$API_PORT/emails" -H "Content-Type: text/plain" -H "Authorization: $AUTHORIZATION" --data-binary @- || exit 75
+cat | curl -X POST "http://localhost:$API_PORT/emails" -H "Content-Type: text/plain" -u "$USERNAME:$PASSWORD" --data-binary @- || exit 75
