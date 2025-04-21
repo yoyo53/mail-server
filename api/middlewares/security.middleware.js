cd8c2b48eaf453c45e99bfade401803e900c2602
Yohan Villiers <94387950+yoyo53@users.noreply.github.com>
2025-04-21 18:40:25 +0200

    Update security.middleware.js

--- api/middlewares/security.middleware.js
+++ api/middlewares/security.middleware.js
@@ -1,10 +1,18 @@
 function verifyAuthorization(request, response, next) {
-    const authorization = request.get("Authorization");
-    if (authorization === process.env.AUTHORIZATION) {
-        next();
+    const auth = req.headers.authorization;
+    if (!auth) {
+        res.set('WWW-Authenticate', 'Basic realm="Secure Area"');
+        return res.status(401).send('Authentication required.');
+    }
+
+    const [username, password] = Buffer.from(auth.split(' ')[1], 'base64').toString().split(':');
+    if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+        return next();
     } else {
-        response.status(401).json({ error: "Unauthorized" });
+        res.set('WWW-Authenticate', 'Basic realm="Secure Area"');
+        return res.status(401).send('Invalid credentials.');
     }
 }
+
 
 module.exports = { verifyAuthorization };
